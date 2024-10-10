a1fd46c3019a2218b2d8018afa8c7121f4d21138
张昭骏 <89625845+Cicecoo@users.noreply.github.com>
2024-10-10 11:12:38 +0800

    update: 添加dvc预处理；跑通训练

---
+++ dvc_dataset.py
@@ -0,0 +1,71 @@
+'''
+参考
+https://github.com/ZhihaoHu/PyTorchVideoCompression/blob/master/DVC/dataset.py
+'''
+import os
+import torch
+import imageio
+import numpy as np
+import torch.utils.data as data
+# from DVC.subnet.basics import *
+# from DVC.subnet.ms_ssim_torch import ms_ssim
+from DVC.augmentation import random_flip, random_crop_and_pad_image_and_labels
+
+# 以下参数来自 DCVC_net.py
+out_channel_mv = 128
+out_channel_N = 64  
+out_channel_M = 96
+
+vimeo_data_path = 'H:/Data/vimeo_septuplet/vimeo_septuplet/sequences/'
+vimeo_test_list_path = 'H:/Data/vimeo_septuplet/vimeo_septuplet/mini_dvc_test.txt'
+
+class DataSet(data.Dataset):
+    def __init__(self, path=vimeo_test_list_path, im_height=256, im_width=256):
+        self.image_input_list, self.image_ref_list = self.get_vimeo(filefolderlist=path)
+        self.im_height = im_height
+        self.im_width = im_width
+        
+        self.featurenoise = torch.zeros([out_channel_M, self.im_height // 16, self.im_width // 16])
+        self.znoise = torch.zeros([out_channel_N, self.im_height // 64, self.im_width // 64])
+        self.mvnois = torch.zeros([out_channel_mv, self.im_height // 16, self.im_width // 16])
+        print("dataset find image: ", len(self.image_input_list))
+
+    # TODO: 改为使用全部数据？
+    def get_vimeo(self, rootdir=vimeo_data_path, filefolderlist=vimeo_test_list_path):
+        with open(filefolderlist) as f:
+            data = f.readlines()
+            
+        fns_train_input = []
+        fns_train_ref = []
+
+        for n, line in enumerate(data, 1):
+            y = os.path.join(rootdir, line.rstrip())
+            fns_train_input += [y]
+            refnumber = int(y[-5:-4]) - 2
+            refname = y[0:-5] + str(refnumber) + '.png'
+            fns_train_ref += [refname]
+
+        return fns_train_input, fns_train_ref
+
+    def __len__(self):
+        return len(self.image_input_list)
+
+    def __getitem__(self, index):
+        input_image = imageio.imread(self.image_input_list[index])
+        ref_image = imageio.imread(self.image_ref_list[index])
+
+        input_image = input_image.astype(np.float32) / 255.0
+        ref_image = ref_image.astype(np.float32) / 255.0
+
+        input_image = input_image.transpose(2, 0, 1)
+        ref_image = ref_image.transpose(2, 0, 1)
+        
+        input_image = torch.from_numpy(input_image).float()
+        ref_image = torch.from_numpy(ref_image).float()
+
+        input_image, ref_image = random_crop_and_pad_image_and_labels(input_image, ref_image, [self.im_height, self.im_width])
+        input_image, ref_image = random_flip(input_image, ref_image)
+
+        quant_noise_feature, quant_noise_z, quant_noise_mv = torch.nn.init.uniform_(torch.zeros_like(self.featurenoise), -0.5, 0.5), torch.nn.init.uniform_(torch.zeros_like(self.znoise), -0.5, 0.5), torch.nn.init.uniform_(torch.zeros_like(self.mvnois), -0.5, 0.5)
+        return input_image, ref_image, quant_noise_feature, quant_noise_z, quant_noise_mv
+        
--- train.py
+++ train.py
@@ -8,7 +8,9 @@ from torch.nn import Module
 from src.models.DCVC_net import DCVC_net
 from src.zoo.image import model_architectures as architectures
 from test_video import PSNR, ms_ssim, read_frame_to_torch
+from dvc_dataset import DataSet
 
+import wandb
 
 train_args = {
     'i_frame_model_name': "cheng2020-anchor",
@@ -30,6 +32,22 @@ train_args = {
     "quality": 3,
     "gop": 10,
 }
+
+save_folder = "runs/"
+if not os.path.exists(save_folder):
+    os.makedirs(save_folder)
+
+# 检测并按序号新建文件夹 train0, train1, ...
+sub_dir_name = "train"
+# 检查当前目录下是否有 train0, train1, ... 文件夹
+sub_dir_index = 0
+while os.path.exists(os.path.join(save_folder, sub_dir_name + str(sub_dir_index))):
+    sub_dir_index += 1
+# 新建文件夹
+sub_dir_name = sub_dir_name + str(sub_dir_index)
+save_folder = os.path.join(save_folder, sub_dir_name)
+os.makedirs(save_folder)
+
 
 # 1.mv warmup; 2.train excluding mv; 3.train excluding mv with bit cost; 4.train all
 borders_of_steps = [1, 4, 7]  
@@ -66,10 +84,10 @@ class Trainer(Module):
             load_checkpoint = torch.load(args['dcvc_model_path'], map_location=torch.device('cpu'))
             self.video_net.load_dict(load_checkpoint)
 
-        device = torch.device("cuda" if args['cuda'] else "cpu")
-        self.i_frame_net.to(device)
+        self.device = torch.device("cuda" if args['cuda'] else "cpu")
+        self.i_frame_net.to(self.device)
         self.i_frame_net.eval()
-        self.video_net.to(device)
+        self.video_net.to(self.device)
 
         self.optimizer = optim.Adam(self.video_net.parameters(), lr=1e-4)
 
@@ -124,7 +142,7 @@ class Trainer(Module):
             loss_settings["components"].append("frame_prior_rate") # zt
         self.loss_settings = loss_settings
 
-        if self.current_epoch == borders_of_steps[3]:
+        if self.current_epoch == borders_of_steps[2]:
             self.optimizer.param_groups[0]["lr"] = 1e-5
             
     """ 
@@ -160,75 +178,121 @@ class Trainer(Module):
             loss += net_output[self.loss_setting2output_obj[component]]
         return loss
 
-
     def training_step(self, batch, batch_idx):
         self.schedule()
+
+        input_image, ref_image, quant_noise_feature, quant_noise_z, quant_noise_mv = batch
+        input_image = input_image.to(self.device)
+        ref_image = ref_image.to(self.device)
+        output = self.video_net.forward(referframe=ref_image, input_image=input_image)
+
+        loss = self.loss(output, ref_image)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        if train_args['model_type'] == 'psnr':
+            quality = PSNR(output['recon_image'], ref_image)
+        else:
+            quality = ms_ssim(output['recon_image'], ref_image, data_range=1.0).item()
+
+        return loss, quality
         
-        ref_frame = None
-        # frame_types = [] # 看过 h264 后理解，用于记录帧类型（I、P），给出解码顺序
-        qualitys = [] # PSNR 或 MS-SSIM，给出每帧的压缩？质量
-        # bits = [] # 对于 I 帧，记录 bit.item()；对于 P 帧，bpp.item()*frame_pixel_num。即每帧的比特数
-        # bits_mv_y = [] 
-        # bits_mv_z = []
-        # bits_y = [] 
-        # bits_z = []
-        frame_pixel_num = 0 
-        frame_num = args_dict['frame_num']
 
-        for frame_idx in range(frame_num):
-            # 读取一帧
-            ori_frame = read_frame_to_torch(
-                os.path.join(args_dict['dataset_path'],
-                             sub_dir_name,
-                             f"im{str(frame_idx+1).zfill(padding)}.png"))
-            ori_frame = ori_frame.to(self.device) # 保证设备一致
-
-            if frame_pixel_num == 0:
-                frame_pixel_num = ori_frame.shape[2]*ori_frame.shape[3] # shape 为 [batch, channel, height, width]， 见 read_frame_to_torch 
-            else: # 每帧 比较前后帧尺寸（保证一致）
-                assert(frame_pixel_num == ori_frame.shape[2]*ori_frame.shape[3])
-
-            if frame_idx % self.gop == 0: # I 帧
-                with torch.no_grad():
-                    result = self.i_frame_net(ori_frame)
-                # bit = sum((torch.log(likelihoods).sum() / (-math.log(2)))  # bit = Sigma(Sigma(log(likelihoods))) / (-log(2)) = - Sigma Sigma(log2(likelihoods))
-                #           for likelihoods in result["likelihoods"].values()) # likelihoods 是什么样子（为什么是二维）？
-                ref_frame = result["x_hat"] # 为 P 帧初始化参考帧
-                    
-                # frame_types.append(0)
-                # bits.append(bit.item())
-                # bits_mv_y.append(0)
-                # bits_mv_z.append(0)
-                # bits_y.append(0)
-                # bits_z.append(0)
-            else: # P 帧
-                result = self.video_net(ref_frame, ori_frame)
-                ref_frame = result['recon_image'] # 由于没有B帧，下一帧如果需要参考帧，一定是上一帧，对于 P 帧是重建后的帧？
-
-                loss = self.loss(result, ori_frame)
-
-                # TODO: froze 部分网络
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-                 
-                # bpp = result['bpp']
-                # frame_types.append(1)
-                # bits.append(bpp.item()*frame_pixel_num)
-                # bits_mv_y.append(result['bpp_mv_y'].item()*frame_pixel_num)
-                # bits_mv_z.append(result['bpp_mv_z'].item()*frame_pixel_num)
-                # bits_y.append(result['bpp_y'].item()*frame_pixel_num)
-                # bits_z.append(result['bpp_z'].item()*frame_pixel_num)
-
-            ref_frame = ref_frame.clamp_(0, 1) # clamp 限幅
-
-            if train_args['model_type'] == 'psnr':
-                qualitys.append(PSNR(ref_frame, ori_frame))
-            else:
-                qualitys.append(ms_ssim(ref_frame, ori_frame, data_range=1.0).item())
- 
     def validation_step(self, batch, batch_idx):
+        input_image, ref_image, quant_noise_feature, quant_noise_z, quant_noise_mv = batch
         with torch.no_grad():
-            pass
+            output = self.video_net.forward(referframe=ref_image, input_image=input_image)
+            loss = self.loss(output, ref_image)
+            
+            if train_args['model_type'] == 'psnr':
+                quality = PSNR(output['recon_image'], ref_image)
+            else:
+                quality = ms_ssim(output['recon_image'], ref_image, data_range=1.0).item()
+
+        return loss, quality
+
+
+if __name__ == "__main__":
+    wandb.init(project="DCVC-Trainer")
+    wandb.config.update(train_args)
+
+    trainer = Trainer(train_args)
+    dataset = DataSet()
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=train_args['batch_size'], shuffle=True, num_workers=train_args['worker'])
+    for epoch in range(12):
+        trainer.current_epoch = epoch
+        for batch_idx, (input_image, ref_image, quant_noise_feature, quant_noise_z, quant_noise_mv) in enumerate(dataloader):
+            loss, quality = trainer.training_step((input_image, ref_image, quant_noise_feature, quant_noise_z, quant_noise_mv), batch_idx)
+            
+            
+            wandb.log({"loss": loss, "quality": quality})
+            wandb.log({"epoch": epoch, "batch": batch_idx})
+            
+        print(f"Epoch {epoch}, batch {batch_idx}, loss: {loss}, quality({train_args['model_type']}): {quality}")
+
+        # save model
+        torch.save(trainer.video_net.state_dict(), os.path.join(save_folder, f"model_epoch_{epoch}.pth"))
+        
+
+    # def training_step(self, batch, batch_idx):
+    #     self.schedule()
+    #     ref_frame = None
+    #     qualitys = [] # PSNR 或 MS-SSIM，给出每帧的压缩？质量
+    #     # bits = [] # 对于 I 帧，记录 bit.item()；对于 P 帧，bpp.item()*frame_pixel_num。即每帧的比特数
+    #     # bits_mv_y = [] 
+    #     # bits_mv_z = []
+    #     # bits_y = [] 
+    #     # bits_z = []
+    #     frame_pixel_num = 0 
+    #     frame_num = args_dict['frame_num']
+
+    #     for frame_idx in range(frame_num):
+    #         # 读取一帧
+    #         ori_frame = read_frame_to_torch(
+    #             os.path.join(args_dict['dataset_path'],
+    #                          sub_dir_name,
+    #                          f"im{str(frame_idx+1).zfill(padding)}.png"))
+    #         ori_frame = ori_frame.to(self.device) # 保证设备一致
+
+    #         if frame_pixel_num == 0:
+    #             frame_pixel_num = ori_frame.shape[2]*ori_frame.shape[3] # shape 为 [batch, channel, height, width]， 见 read_frame_to_torch 
+    #         else: # 每帧 比较前后帧尺寸（保证一致）
+    #             assert(frame_pixel_num == ori_frame.shape[2]*ori_frame.shape[3])
+
+    #         if frame_idx % self.gop == 0: # I 帧
+    #             with torch.no_grad():
+    #                 result = self.i_frame_net(ori_frame)
+    #             ref_frame = result["x_hat"]
+    #         else: # P 帧
+    #             result = self.video_net(ref_frame, ori_frame)
+    #             ref_frame = result['recon_image'] 
+
+    #             loss = self.loss(result, ori_frame)
+
+    #             # TODO: freeze 部分网络
+    #             self.optimizer.zero_grad()
+    #             loss.backward()
+    #             self.optimizer.step()
+                 
+    #             # bpp = result['bpp']
+    #             # frame_types.append(1)
+    #             # bits.append(bpp.item()*frame_pixel_num)
+    #             # bits_mv_y.append(result['bpp_mv_y'].item()*frame_pixel_num)
+    #             # bits_mv_z.append(result['bpp_mv_z'].item()*frame_pixel_num)
+    #             # bits_y.append(result['bpp_y'].item()*frame_pixel_num)
+    #             # bits_z.append(result['bpp_z'].item()*frame_pixel_num)
+
+    #         ref_frame = ref_frame.clamp_(0, 1) # clamp 限幅
+
+    #         if train_args['model_type'] == 'psnr':
+    #             qualitys.append(PSNR(ref_frame, ori_frame))
+    #         else:
+    #             qualitys.append(ms_ssim(ref_frame, ori_frame, data_range=1.0).item())
+ 
+
+    #     with torch.no_grad():
+    #         pass
 
 
